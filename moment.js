86e230afb2e361988a9adaeac8f47561f3174066
Serina Tsang <serinat@uw.edu>
2019-10-15 23:31:08 -0700

    updated files

---
+++ moment.js
@@ -0,0 +1,18 @@
+//function to build schedular rows (function buildRows) --> loop, moment.js
+function buildRows() {
+
+}
+//Set date at top of page (function showTodaysDate) --> moment.js
+function showTodaysDate() {
+
+}
+//Save button event handler (function saveRow) --> uses localStorage
+function saveRow() {
+
+}
+//Change row styles (function updateRowStyle) --> moment.js
+function updateRowStyle() {
+    
+}
+ //i. on page load, check current time (hour) against rows in scheduler
+ //ii. update style for those that are past hours, present hours and future hours
