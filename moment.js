b61d5f4dceabfe684f72fe121d882cd2582cd10e
Serina Tsang <serinat@uw.edu>
2019-10-18 01:01:16 -0700

    created rows

--- moment.js
+++ moment.js
@@ -1,7 +1,19 @@
 //function to build schedular rows (function buildRows) --> loop, moment.js
-function buildRows() {
+//var rows = [];
+//function buildRows() {
+    //$("#row").empty();
+    //for (var i = 0; i < 10; i++) {
+        //var a = $("<div id='row'></div>");
+        //$("#row").append(a);
+    //}
+//}
+//buildRows();
 
-}
+//var $table = $('<table/>');
+//for(var i=0; i<10; i++){
+//$table.append( '<tr><td>' + 'result' +  i + '</td></tr>' );
+//}
+//$('.row').append($table);
 //Set date at top of page (function showTodaysDate) --> moment.js
 function showTodaysDate() {
 
@@ -10,9 +22,10 @@ function showTodaysDate() {
 function saveRow() {
 
 }
+
 //Change row styles (function updateRowStyle) --> moment.js
 function updateRowStyle() {
-    
+
 }
  //i. on page load, check current time (hour) against rows in scheduler
  //ii. update style for those that are past hours, present hours and future hours
